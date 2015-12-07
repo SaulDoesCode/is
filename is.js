4575d1f5f9fdb0ce2bb40445b957a6d00800613d
Saul van der Walt <saulvdw@gmail.com>
2015-12-07 18:25:26 +0200

    fixed bug

--- is.js
+++ is.js
@@ -1,8 +1,19 @@
   "use strict";
 (() => {
-    let type = (obj, str) => toString.call(obj) === str,
-    isT = (val, str) => typeof val === str,
-    nT = (val, str) => !isT(val, str);
+  let type = (obj, str) => toString.call(obj) === str,
+  isT = (val, str) => typeof val === str,
+  nT = (val, str) => !isT(val, str),
+  eachisInstanceof = (test,collection) => {
+    if(isT(collection, 'string') || collection === undefined || collection === null) return false;
+    let allgood = true, i = 0;
+    for (; i < collection.length; i++) {
+      if(collection[i] instanceof test) {
+        allgood = false;
+        break;
+      }
+    }
+    return allgood;
+  };
 
   self.is = {
     Bool: val => typeof val === 'boolean',
@@ -13,21 +24,17 @@
     },
     String: val => isT(val, 'string'),
     Num: val => isT(val, 'number'),
-    Undef: (...args) => args.every(o => isT(o, 'undefined')),
-    Def: (...args) => args.every(o => nT(o, 'undefined')),
-    Null: (...args) => args.every(o => o === null),
-    Node: (...args) => args.every(o => o instanceof Node),
-    NodeList: (...args) => {
-      for (let i = 0; i < args.length; i++)
-        if (Array.from(args[i]).every(n => is.Node(n))) return true;
-      return false;
-    },
-    Object: (...args) => args.every(o => type(o, '[object Object]')),
-    Element: (...args) => args.every(o => type(o, '[object HTMLElement]')),
-    File: (...args) => args.every(o => type(o, '[object File]')),
-    FormData: (...args) => args.every(o => type(o, '[object FormData]')),
-    Map: (...args) => args.every(o => type(o, '[object Map]')),
-    Func: (...args) => args.every(o => typeof o === 'function'),
+    Undef: (...args) => args.length && args.every(o => isT(o, 'undefined')),
+    Def: (...args) => args.length && args.every(o => nT(o, 'undefined')),
+    Null: (...args) => args.length && args.every(o => o === null),
+    Node: (...args) => args.length && args.every(o => o instanceof Node),
+    NodeList: (...args) => args.length ? args.every(n => n === null ? false : n instanceof NodeList || eachisInstanceof(Node,n)) : false,
+    Object: (...args) => args.length && args.every(o => type(o, '[object Object]')),
+    Element: (...args) => args.length && args.every(o => type(o, '[object HTMLElement]')),
+    File: (...args) => args.length && args.every(o => type(o, '[object File]')),
+    FormData: (...args) => args.length && args.every(o => type(o, '[object FormData]')),
+    Map: (...args) => args.length && args.every(o => type(o, '[object Map]')),
+    Func: (...args) => args.length && args.every(o => typeof o === 'function'),
     Blob: obj => type(obj, '[object Blob]'),
     RegExp: obj => type(obj, '[object RegExp]'),
     Date: obj => type(obj, '[object Date]'),
@@ -44,5 +51,5 @@
       let type = typeof val;
       return type === 'function' ? RegExp('^' + String(Object.prototype.toString).replace(/[.*+?^${}()|[\]\/\\]/g, '\\$&').replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?') + '$').test(Function.prototype.toString.call(val)) : (val && type == 'object' && /^\[object .+?Constructor\]$/.test(val.toString)) || false;
     },
-  }
+  };
 })();
